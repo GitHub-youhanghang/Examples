abd974b72acba60652cb572b7b4db2a26d6b0dee
GitHub-youhanghang <youhanghang9527@gmail.com>
2017-08-14 13:53:18 +0800

    flex布局

--- js/showCode.js
+++ js/showCode.js
@@ -1,21 +1,10 @@
     $(document).ready(function() {
         var styleCon = $('style').html();
         var bodyCon = $('body').html();
-        $('body').append('<textarea id="styleSrc"></textarea>');
-        $('body').append('<textarea id="bodySrc"></textarea>');
-        $('<button id="srcBtn" type="button" class="btn btn-primary btn-lg">关闭源代码</button>').insertBefore($('body'));  
-        $('textarea').css({
-            position: 'fixed',
-            right: 0,
-            width: '50%',
-            height: '50%'
-        })   
-        $('#bodySrc').css({
-            top: 0
-        });        
-        $('#styleSrc').css({
-            top: '50%'
-        }); 
+        $('body').append('<div class="showCodeBox bottom"><textarea id="styleSrc"></textarea><textarea id="bodySrc"></textarea></div>');
+
+        $('body').append('<div class="srcBtns"><button id="srcBtn"  class="btn btn-primary">关闭源代码</button><button id="codeBoxPosition"  class="btn btn-primary bottom">源代码位置-右</button></div>');
+
 
         $('#bodySrc').val(bodyCon);
         $('#styleSrc').val(styleCon);
@@ -24,14 +13,286 @@
         $('#srcBtn').on('click',function(){
             if (srcIsOpen) {
                 $(this).text('打开源代码');
-
                 $('textarea').hide();
                 srcIsOpen=false;
+                $('body').removeClass('body-padding-right');
+                  $('#codeBoxPosition').hide();
             }else{
                 $(this).text('关闭源代码');
                  $('textarea').show();
-                srcIsOpen=true;            
+                srcIsOpen=true;
+                $('#codeBoxPosition').show();
+                if (srcPosition=='right') {
+                  $('#codeBoxPosition').text('源代码位置-右');
+                  $('.showCodeBox').removeClass('right').addClass('bottom');
+                  srcPosition='bottom';
+                  $('body').removeClass('body-padding-right');
+                }
+            }
+        })
+
+        var srcPosition='bottom';
+        $('#codeBoxPosition').on('click',function(){
+            if (srcPosition=='bottom') {
+                $(this).text('源代码位置-下');
+                $('.showCodeBox').removeClass('bottom').addClass('right');
+                srcPosition='right';
+                $('body').addClass('body-padding-right');
+            }else{
+                $(this).text('源代码位置-右');
+                $('.showCodeBox').removeClass('right').addClass('bottom');
+                srcPosition='bottom';
+                $('body').removeClass('body-padding-right');
             }
         })
 
     });
+
+
+
+
+    /*             返回顶部
+ *
+ * Copyright (c) 2014 Daniele Lenares (https://github.com/Ryuk87)
+ * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
+ * and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
+ *
+ * Version 0.5.1
+ *
+ */
+(function ( $ ) {
+
+	$.goup = function(user_params) {
+
+		/* Default Params */
+		var params = $.extend({
+				location : 'right',
+				locationOffset : 20,
+				bottomOffset : 10,
+				containerRadius : 10,
+				containerClass : 'goup-container',
+				arrowClass : 'goup-arrow',
+				alwaysVisible : false,
+				trigger: 500,
+				entryAnimation : 'fade',
+				goupSpeed : 'slow',
+				hideUnderWidth : 500,
+				containerColor : '#000',
+				arrowColor : '#fff',
+                title : '',
+                titleAsText : false,
+                titleAsTextClass : 'goup-text'
+			}, user_params);
+		/* */
+
+
+		$('body').append('<div style="display:none" class="'+params.containerClass+'"></div>');
+		var container = $('.'+params.containerClass);
+		$(container).html('<div class="'+params.arrowClass+'"></div>');
+		var arrow = $('.'+params.arrowClass);
+
+		/* Parameters check */
+		var location = params.location;
+		if (location != 'right' && location != 'left') {
+			location = 'right';
+		}
+
+		var locationOffset = params.locationOffset;
+		if (locationOffset < 0) {
+			locationOffset = 0;
+		}
+
+		var bottomOffset = params.bottomOffset;
+		if (bottomOffset < 0) {
+			bottomOffset = 0;
+		}
+
+		var containerRadius = params.containerRadius
+		if (containerRadius < 0) {
+			containerRadius = 0;
+		}
+
+		var trigger = params.trigger;
+		if (trigger < 0) {
+			trigger = 0;
+		}
+
+		var hideUnderWidth = params.hideUnderWidth;
+		if (hideUnderWidth < 0) {
+			hideUnderWidth = 0;
+		}
+
+		var checkColor = /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i;
+		if (checkColor.test(params.containerColor)) {
+			var containerColor = params.containerColor;
+		} else {
+			var containerColor = '#000';
+		}
+		if (checkColor.test(params.arrowColor)) {
+			var arrowColor = params.arrowColor;
+		} else {
+			var arrowColor = '#fff';
+		}
+
+        if (params.title === '') {
+            params.titleAsText = false;
+        }
+		/* */
+
+		/* Container Style */
+		var containerStyle = {};
+		containerStyle = {
+			position : 'fixed',
+			width : 40,
+			height : 40,
+			background : containerColor,
+			cursor: 'pointer'
+		};
+		containerStyle['bottom'] = bottomOffset;
+		containerStyle[location] = locationOffset;
+		containerStyle['border-radius'] = containerRadius;
+
+		$(container).css(containerStyle);
+        if (!params.titleAsText) {
+            $(container).attr('title', params.title);
+        } else {
+            $('body').append('<div class="'+params.titleAsTextClass+'">'+params.title+'</div>');
+            var textContainer = $('.'+params.titleAsTextClass);
+            $(textContainer).attr('style', $(container).attr('style'));
+            $(textContainer).css('background','transparent')
+                           .css('width',80)
+                           .css('height','auto')
+                           .css('text-align','center')
+                           .css(location,locationOffset - 20);
+            var containerNewBottom = $(textContainer).height() + 10;
+            $(container).css('bottom', '+='+containerNewBottom+'px');
+        }
+
+
+		/* Arrow Style */
+		var arrowStyle = {};
+		arrowStyle = {
+			width : 0,
+			height : 0,
+			margin : '0 auto',
+			'padding-top' : 13,
+			'border-style' : 'solid',
+			'border-width' : '0 10px 10px 10px',
+			'border-color' : 'transparent transparent '+arrowColor+' transparent'
+		};
+		$(arrow).css(arrowStyle);
+		/* */
+
+
+
+		/* Trigger Hide under a certain width */
+		var isHidden = false;
+		$(window).resize(function(){
+			if ($(window).outerWidth() <= hideUnderWidth) {
+				isHidden = true;
+				do_animation($(container), 'hide', params.entryAnimation);
+                if (textContainer)
+                    do_animation($(textContainer), 'hide', params.entryAnimation);
+			} else {
+				isHidden = false;
+				$(window).trigger('scroll');
+			}
+		});
+		/* If i load the page under a certain width, i don't have the event 'resize' */
+		if ($(window).outerWidth() <= hideUnderWidth) {
+			isHidden = true;
+			$(container).hide();
+            if (textContainer)
+                $(textContainer).hide();
+		}
+
+
+		/* Trigger show event */
+		if (!params.alwaysVisible) {
+			$(window).scroll(function(){
+				if ($(window).scrollTop() >= trigger && !isHidden) {
+					do_animation($(container), 'show', params.entryAnimation);
+                    if (textContainer)
+                        do_animation($(textContainer), 'show', params.entryAnimation);
+				}
+
+				if ($(window).scrollTop() < trigger && !isHidden) {
+					do_animation($(container), 'hide', params.entryAnimation);
+                    if (textContainer)
+                        do_animation($(textContainer), 'hide', params.entryAnimation);
+				}
+			});
+		} else {
+			do_animation($(container), 'show', params.entryAnimation);
+            if (textContainer)
+                do_animation($(textContainer), 'show', params.entryAnimation);
+		}
+		/* If i load the page and the scroll is over the trigger, i don't have immediately the event 'scroll' */
+		if ($(window).scrollTop() >= trigger && !isHidden) {
+			do_animation($(container), 'show', params.entryAnimation);
+            if (textContainer)
+                do_animation($(textContainer), 'show', params.entryAnimation);
+		}
+
+		/* Click event */
+		$(container).on('click', function(){
+			$('html,body').animate({ scrollTop: 0 }, params.goupSpeed);
+			return false;
+		});
+
+        $(textContainer).on('click', function(){
+			$('html,body').animate({ scrollTop: 0 }, params.goupSpeed);
+			return false;
+		});
+	};
+
+
+	/* Private function for the animation */
+	function do_animation(obj, type, animation) {
+		if (type == 'show') {
+			switch(animation) {
+				case 'fade':
+					obj.fadeIn();
+				break;
+
+				case 'slide':
+					obj.slideDown();
+				break;
+
+				default:
+					obj.fadeIn();
+			}
+		} else {
+			switch(animation) {
+				case 'fade':
+					obj.fadeOut();
+				break;
+
+				case 'slide':
+					obj.slideUp();
+				break;
+
+				default:
+					obj.fadeOut();
+			}
+		}
+	}
+
+}( jQuery ));
+
+//调用返回顶部插件
+$(document).ready(function() {
+      $.goup({
+
+            trigger: 100,
+
+            bottomOffset: 150,
+
+            locationOffset: 100,
+
+            title: 'TOP',
+
+            titleAsText: true
+
+        });
+});
